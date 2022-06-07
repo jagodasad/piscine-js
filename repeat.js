64490c9da4cfb02e03af948d16747d32fa3d73a5
jagodasad <jago.sadowska@gmail.com>
2022-06-07 15:10:17 +0100

    repeat amount

---
+++ repeat.js
@@ -0,0 +1,13 @@
+function repeat(str, number) {
+    const a = str
+    if (number <= 0) {
+        return ''
+    }
+    while (number !== 1 ) {
+        str += a
+        count --
+    }
+    return str
+}
+
+// console.log(repeat('hello', 5))
