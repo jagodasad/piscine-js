9584ae4b6b113c9f88c3d1b7db83db11d5901c08
jagodasad <jago.sadowska@gmail.com>
2022-06-07 10:24:16 +0100

    value properties

---
+++ is.js
@@ -0,0 +1,11 @@
+is.num = (a) => typeof a === 'number';
+is.nan = (a) => Number.isNaN(a);
+is.str = (a) => typeof a === 'string';
+is.bool = (a) => typeof a === 'boolean';
+is.undef = (a) =>  a === undefined;
+is.def = (a) => is.def(a);
+is.arr = (a) => Array.isArray(a);
+is.obj = (a) => typeof a === 'object' && !is.fun(a) && !is.arr(a) && a !== null;
+is.fun = (a) => typeof a === 'function';
+is.truthy = (a) => !!a;
+is.falsy = (a) => !a;
