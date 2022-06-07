4f558934e78bcb000f516cdddd19e776e6b6d575
jagodasad <jago.sadowska@gmail.com>
2022-06-07 11:13:38 +0100

    newton

--- physics.js
+++ physics.js
@@ -1,15 +1,17 @@
 const x = (f,m) => f/m;
-const y = (v,t) => v/t;
-const z = (d,t) => (2 * d) / (t * t);
 
-function getAccelaration(obj) {
-    if (!Number.isNaN(x(obj.f, obj.m))) {
-        return x(obj.f, obj.m)
-    } else if (!Number.isNaN(y(obj.Δv, obj.Δt))) {
-        return y(obj.Δv, obj.Δt)
-    } else if (!Number.isNaN(z(obj.d, obj.t))) {
-        return z(obj.d, obj.t)
+const y = (v,t) => v/t;
+
+const z = (d,t) => (2*d)/(t*t);
+
+function getAcceleration(obj) {
+    if (!Number.isNaN(x(obj.f,obj.m))) {
+        return x(obj.f,obj.m)
+    } else if (!Number.isNaN(y(obj.Δv,obj.Δt))) {
+        return y(obj.Δv,obj.Δt)   
+    } else if (!Number.isNaN(z(obj.d,obj.t))) {
+        return z(obj.d,obj.t)
     }
-    return "impossible"    
-    
+
+    return "impossible"
 }
