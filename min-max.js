f222781e72a5282f4a951be573f77563b1b2b7bc
jagodasad <jago.sadowska@gmail.com>
2022-06-07 09:43:28 +0100

    mix max

---
+++ min-max.js
@@ -0,0 +1,19 @@
+function max(x,z) {
+    if (x > z) {
+        return x
+    }
+    if (z > x) {
+        return z
+    }
+}
+
+function min(x,z) {
+    if (x > z) {
+        return z
+    }
+    if (z > x) {
+        return x
+    }
+}
+
+// console.log(max(140, 340))
