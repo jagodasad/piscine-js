3c7cd56fe3fbb3a0ce50b0594da8400cfe50dfed
jagodasad <jago.sadowska@gmail.com>
2022-06-09 15:57:22 +0100

    string and 2regExp

--- same-amount.js
+++ same-amount.js
@@ -1,3 +1,5 @@
-function sameAmount() {
-    
+function sameAmount(str, reg1, reg2) {
+    reg1 = new RegExp(reg1.source, reg1.flags + "g")
+    reg2 = new RegExp(reg2.source, reg2.flags + "g")
+    return
 }
