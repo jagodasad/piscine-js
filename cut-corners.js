58e7e0fa04ac0d45a9d524f02f95513fa3208eb1
jagodasad <jago.sadowska@gmail.com>
2022-06-09 08:22:59 +0100

    math round etc

--- cut-corners.js
+++ cut-corners.js
@@ -1,35 +1,25 @@
-function modulo(a, b) {
-    let isNegative = false
-
-    if (a < 0 && b < 0) {
-        isNegative = true
-        a = multiply(a, -1)
-        b = multiply(b, -1)
-
-    } else if (a < 0) {
-        isNegative = true
-        a = multiply(a, -1)
-
-    } else if (b < 0) {
-        b = multiply(b, -1)
-
-    } else {
-        if (a < b) {
-            return a
-        }
+const modulo = (a, b) => {
+    let s1 = 1;
+    let s2 = 1;
+    if (b < 0) {
+        b = -b;
+        // s1 = -1;
     }
-
-    while (a !== 1) {
-        a -= b
-        if (a < b) {
-
-            return isNegative ? a == 0 ? 0 : multiply(a, -1) : a
-        }
+    if (a < 0) {
+        a = -a;
+        s2 = -1;
     }
-
-    return isNegative ? a == 0 ? 0 : multiply(a, -1) : a
-
-}
+    while (a >= b) {
+        a -= b;
+    }
+    if (s1 < 0 && s2 < 0) {
+        return a;
+    }
+    if (s1 < 0 || s2 < 0) {
+        return -a;
+    }
+    return a;
+};
 
 const trun = (n) => {
     let m = modulo(n, 1);
@@ -66,7 +56,4 @@ function floor(num) {
     if (num > 0 || num === trunc(num)) return trunc(num)
     else return trunc(num) -1
 }
-
-
-
 
