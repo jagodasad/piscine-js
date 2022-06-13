6301d07b6d863f9e1f23f5bd9a9e9c66cdcbd812
jagodasad <jago.sadowska@gmail.com>
2022-06-13 11:14:34 +0100

    date is future, past etc

---
+++ date-is.js
@@ -0,0 +1,35 @@
+const isValid = (date) => {
+    let k = new Date(date)
+    if (date.toString() !== 'Invalid Date'){
+        return true
+    }
+    return false
+}
+
+function isAfter(date1, date2) {
+    if (!isValid(date1)) return false
+    if (!isValid(date2)) return false
+
+    return date1 > date2
+}
+
+function isBefore(date1, date2) {
+    if (!isValid(date1)) return false
+    if (!isValid(date2)) return false
+
+    return date1 < date2
+}
+
+function isFuture(date) {
+    if (!isValid(date)) return false
+
+    return date > Date.now()
+}
+
+function isPast(date) {
+    if (!isValid(date)) return false
+
+    return date < Date.now()
+}
+
+// console.log(isValid(''))
