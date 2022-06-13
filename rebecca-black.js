73ef047692e8254efb95d307ac58e4551d33d501
jagodasad <jago.sadowska@gmail.com>
2022-06-13 13:10:00 +0100

    get Fri, weekend, leap year, last day on month

---
+++ rebecca-black.js
@@ -0,0 +1,51 @@
+function getWeekDay(date) {
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay()];
+}
+
+function isFriday(date) {
+    let friday = getWeekDay(date)
+    if(friday = 'Friday') {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isWeekend(date) {
+    let friday = getWeekDay(date)
+    if(friday === 'Saturday' || friday === 'Sunday') {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isLeapYear(date){
+    var result; 
+    date = parseInt(document.getElementById("isYear").value);
+    if (years/400){
+      result = true
+    }
+    else if(years/100){
+      result = false
+    }
+    else if(years/4){
+      result= true
+    }
+    else{
+      result= false
+    }
+    return result
+ }
+
+function isLastDayOfMonth(date) {
+    let month = date.getMonth()
+    date.setHours(30)
+    let getMonth = date.getMonth()
+    if(month != getMonth) {
+        return true
+    } else {
+        return false
+    }
+}
