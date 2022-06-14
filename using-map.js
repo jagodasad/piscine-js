461f2945667957ed1666ab31feb90e7a99ca17a0
jagodasad <jago.sadowska@gmail.com>
2022-06-14 10:58:16 +0100

    uppercase 1st letter, fahrenheit to c

---
+++ using-map.js
@@ -0,0 +1,35 @@
+function citiesOnly(arr) {
+    return arr.map(el => el.city)
+}
+
+function upperCasingStates(arr) {
+    return arr.map(state => {
+        return state.split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        }).join(' ')
+    })
+}
+
+function fahrenheitToCelsius(arr) {
+    return arr.map(el => {
+        return `${Math.floor((Number(el.slice(0, -2)) - 32) * (5 / 9))}°C`
+    })
+}
+
+function trimTemp(arr) {
+    return arr.map(el => {
+        el['temperature'] = el['temperature'].split(' ').join('')
+        return el
+    })
+}
+
+function tempForecasts(arr) {
+    arr = trimTemp(arr)
+    return arr.map(el => {
+        let temp = `${Math.floor((Number(el['temperature'].slice(0, -2)) - 32) * (5 / 9))}°Celsius`
+        let state = el['state'].split(' ').map(word => {
+            return word.charAt(0).toUpperCase() + word.slice(1)
+        }).join(' ')
+        return `${temp} in ${el.city}, ${state}`
+    })
+}
