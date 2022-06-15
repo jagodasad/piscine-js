34043ec8c6256b9b1b79f2167f09e67555ed82c4
jagodasad <jago.sadowska@gmail.com>
2022-06-15 13:35:42 +0100

    mult2, add3, sub4

--- sweet-curry.js
+++ sweet-curry.js
@@ -1,11 +1,27 @@
-function mult2 (a, b) {
-    return a * b;
+function mult2(a) {
+    return function(b) {
+        return a * b
+    }
   }
 
-  function add3 (a, b, c) {
-    return a + b +c;
+  function add3(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c
+        }
+    }
   }
 
   function sub (a, b, c, d) {
     return a - b - c - d;
+  }
+
+  function sub4(a) {
+    return function(b) {
+        return function(c) {
+            return function(d) {
+                return a - b - c -d
+            }
+        }
+    }
   }
