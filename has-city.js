0a4c283498d9b071f59732fbc87f26299fd4d23b
jagodasad <jago.sadowska@gmail.com>
2022-06-15 14:20:18 +0100

    has city

--- has-city.js
+++ has-city.js
@@ -1,9 +1,9 @@
 function hasCity(country, arr) {
     return function(city) {
         if (arr.indexOf(city) === -1 ){
-            return "is not a city from"
+            return `${city} is not a city from`
         } else {
-            return "is a city from"
+            return `${city} is a city from`
         }
     }
 }
