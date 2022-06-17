4a6d10ac1ccd043978af7c5d4487b496202baac8
jagodasad <jago.sadowska@gmail.com>
2022-06-17 10:38:20 +0100

    symphony

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -2,4 +2,13 @@ export const compose = (event) => {
     let key = event?.key
     let notes = document.getElementsByClassName('note')
 
+    if (key === 'Backspace') {
+        if (notes.length > 0) Array.from(notes).pop().remove()
+    } else if (key === 'Escape') {
+        if (notes.length > 0) {
+            Array.from(document.getElementsByClassName('note')).forEach(el => {
+                el.remove()
+            })
+        }
+    }
 }
