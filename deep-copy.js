902b32f81356fe3017396fbdae2816beed157db5
jagodasad <jago.sadowska@gmail.com>
2022-06-22 14:39:26 +0100

    deep copy

--- deep-copy.js
+++ deep-copy.js
@@ -1,6 +1,14 @@
 function deepCopy(obj) {
-    if (typeof obj !== 'object' || Object.keys(obj).length === 0) return obj
+    if (typeof obj !== 'object' || obj === null) return obj
 
     let copy = {}
     if (Array.isArray(obj)) copy = []
+
+    for (const key in obj) {
+        copy[key] = deepCopy(obj[key])
+
+    if (Object.isFrozen(obj[key]))
+    Object.freeze(copy[key])
+    }
+    return copy
 }
