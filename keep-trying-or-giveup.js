8fd9c23941677020f0f9f69982f9ae2084bac4f3
jagodasad <jago.sadowska@gmail.com>
2022-06-23 17:13:36 +0100

    commit

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,26 +1,26 @@
-const retry = (count, callback) => {
-    let attempts = 1;
-    return _retry = async (...args) => callback(...args)
-        .catch(err => {
-            if (attempts > count) throw err
-            attempts++
-            return _retry(...args)
-        });
-}
-
-
-// function retry(count, callback) {
-//     return async (...args) => {
-//         while (true) {
-//             try {
-//                 return await callback(...args)
-//             } catch (e) {
-//                 if (count === 0) throw e
-//                 count--
-//             }
-//         }
-//     }
+// const retry = (count, callback) => {
+//     let attempts = 1;
+//     return _retry = async (...args) => callback(...args)
+//         .catch(err => {
+//             if (attempts > count) throw err
+//             attempts++
+//             return _retry(...args)
+//         });
 // }
+
+
+function retry(count, callback) {
+    return async (...args) => {
+        while (true) {
+            try {
+                return await callback(...args)
+            } catch (e) {
+                if (count === 0) throw e
+                count--
+            }
+        }
+    }
+}
 
 function timeout(delay, callback) {
     return async (...args) => {
