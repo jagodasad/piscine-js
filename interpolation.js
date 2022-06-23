17988322f5d27d962863619c70a3901201657f7b
jagodasad <jago.sadowska@gmail.com>
2022-06-23 15:16:29 +0100

    commit

--- interpolation.js
+++ interpolation.js
@@ -8,3 +8,14 @@ function interpolation(obj) {
         if (step === obj.step) clearInterval(interval)
     }, obj.duration / obj.step)
 }
+
+// function interpolation({ step, start, end, callback, duration }) {
+//     let x = start;
+//     let y = duration / step;
+//     let timer = setInterval(() => {
+//         callback([x, y]);
+//         x += (end - start) / step
+//         y += duration / step
+//         if (Math.round(x * 100) / 100 === end) clearInterval(timer);
+//     }, duration / step)
+// }
