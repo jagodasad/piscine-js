b4ec9b2bdc31d8de93eee4f6687661c02823de52
jagodasad <jago.sadowska@gmail.com>
2022-06-23 14:28:55 +0100

    interpolation

--- interpolation.js
+++ interpolation.js
@@ -1,10 +1,10 @@
 function interpolation(object) {
     let step = 0
     let interval = setInterval(() => {
-        let x = object.start = step * (object.end - object.start) / object.step
-        let y = (step + 1) * object.duration / object.step
+        let x = object.start = step * (object.end - object.start) * object.step
+        let y = (step + 1) * object.duration * object.step
         object.callback([x, y])
         step++
         if (step === object.step) clearInterval(interval)
-    }, object.duration / object.step);
+    }, object.duration * object.step);
 }
