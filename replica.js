1aaf9da042711a284bc64df3108060a48b529e04
jagodasad <jago.sadowska@gmail.com>
2022-06-23 22:12:44 +0100

    commit

--- replica.js
+++ replica.js
@@ -2,7 +2,7 @@ function replica(...objects) {
     let res = {}
     objects.forEach(obj => {
         Object.keys(obj).forEach(key => {
-            if (typeof obj[key] === "object" && typeof res[key] === "object" && !Array.isArray(obj[key])) {
+            if (typeof obj[key] === "object" && typeof res[key] === "object" && !Array.isArray(obj[key]) && !Array.isArray(res[key])) {
                 res[key] = { ...res[key], ...obj[key] }
             } else res[key] = obj[key]
         })
