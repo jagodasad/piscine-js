ede70923e4807c5154648cc8c952170f99e1e2e3
jagodasad <jago.sadowska@gmail.com>
2022-06-24 07:08:24 +0100

    commit

--- pronoun.js
+++ pronoun.js
@@ -1,3 +1,19 @@
 function pronoun(str) {
-
+    const words = str.split(/[\s,]+/)
+    const res = {}
+    words.forEach((word, index) => {
+        word = word.toLowerCase()
+        if (isPronoun(word)) {
+            if (!res[word]) res[word] = { word: [], count: 0 }
+            if (index !== words.length - 1 && !isPronoun(words[index + 1])) {
+                res[word].word.push(words[index + 1])
+            }
+            res[word].count++
+        }
+    })
+    return res
 }
+
+function isPronoun(str) {
+
+}    
